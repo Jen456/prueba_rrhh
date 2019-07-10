df1e9305da5c1270cbdbd858c8352248c203ecea
Jenn <jjalava456@hotmail.com>
2019-07-10 12:58:26 -0500

    agregando frontend con vuejs cli y webpack

--- login/models.py
+++ login/models.py
@@ -2,10 +2,5 @@ from django.db import models
 
 # Create your models here.
 
-class InicioSesion(models.Model):
-    users= models.CharField(max_length=10)
-    password = models.CharField(max_length=10)
-    ruc = models.CharField(max_length=13)
-    register_date = models.DateTimeField('Registro cliente') # inicio del cliente cuando compre la aplicacion 
 
     
--- login/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers 
-from .models import InicioSesion
-
-class InicioSesionSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = InicioSesion
-        fields = '__all__'
-        
