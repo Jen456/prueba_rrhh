ac2318e68e4286a35099203f94004cc912be5d44
Jenn <jjalava456@hotmail.com>
2019-07-15 10:30:18 -0500

    login con vuejs

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>hola {{value}}</h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'about',
+  computed: {
+    ...mapState(['value']),
+    este_valor() {
+      return this.value
+    }
+  }
+}
+</script>
